8e46fc49e1d956da94a10dceb894061936384a1d
MathLogic <duaqamar213@gmail.com>
2026-03-10 12:06:02 -0400

    Add files via upload
    Day 1 of python

---
+++ day1.py
@@ -0,0 +1,12 @@
+print("Hey!! this is our first day learning python")
+
+print("Math Quiz\n")
+
+x = input("any number\n")
+y = input("any number\n")
+
+#string is use to convert number to text and int is use to convert text to number
+answer = int(x) + int(y)
+print(answer)
+
+
